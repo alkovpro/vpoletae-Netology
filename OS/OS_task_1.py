799d2ed6cb628a9cab11087f9fa73612db2ac6e5
vpoletae <43249448+vpoletae@users.noreply.github.com>
2018-11-24 13:33:02 +0300

    Add files via upload

--- OS/OS_task_1.py
+++ OS/OS_task_1.py
@@ -1,24 +1,27 @@
 # searching for .sql files
 import os
 
-search_string = input('Please enter a string: ')
-search_list = []
+while True:
+    search_string = input('Please enter a string: ')
+    search_list = []
 
-for file in os.listdir('C:\\'):
-    if file.endswith('.sql'):
-        with open(file, 'r') as f:
-            if search_string in f:
-                search_list.append(file)
-            else:
-                continue
-    else:
-        continue
+    for file in os.listdir('C:\\'):
+        if file.endswith('.sql'):
+            file_path = os.path.join('C:\\', file)
+            with open(file_path, 'r') as f:
+                f.read()
+                if search_string in f:
+                    search_list.append(file)
+                else:
+                    continue
+        else:
+            continue
 
-for file in search_list:
-    print(file)
+    for file in search_list:
+        print(file)
 
-print('Total: {}'.format(len(search_list))
-        
+    print('Total: {}'.format(len(search_list))
+            
                        
 
         
