1e4f07f077500abc5a7a25e1f3c90f9f6c5a5346
vpoletae <43249448+vpoletae@users.noreply.github.com>
2019-02-02 20:39:12 +0300

    Add files via upload

--- decorator_logger.py
+++ decorator_logger.py
@@ -1,25 +1,25 @@
-
 # Decorator
-def logger(old_function):
-      def new_function(*args, **kwargs):
-            from time import ctime
-            from datetime import date
-            from inspect import signature
+def param_logger(path: str):
+      def logger(old_function):
+            def new_function(*args, **kwargs):
+                  from time import ctime
+                  from datetime import date
+                  from inspect import signature
             
-            launch_date = date.today()
-            launch_time = ctime()
-            func_name = old_function.__name__
-            func_args = list(signature(old_function).parameters.items())
-            func_output = old_function(*args, **kwargs)
+                  launch_date = date.today()
+                  launch_time = ctime()
+                  func_name = old_function.__name__
+                  func_args = list(signature(old_function).parameters.items())
+                  func_output = old_function(*args, **kwargs)
 
-            data_to_write = [launch_date, launch_time, func_name, func_args, func_output]
+                  data_to_write = [launch_date, launch_time, func_name, func_args, func_output]
 
-            with open('log_file.txt', 'a') as f:
-                  for data in data_to_write:
-                        f.writelines(str(data) + '\n')
-            print (func_output)
-      return new_function
-
+                  with open(path, 'a') as f:
+                        for data in data_to_write:
+                              f.writelines(str(data) + '\n')
+                  print (func_output)
+            return new_function
+      return logger
 
 # Func to decorate
 @logger
