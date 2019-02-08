66ee68e4a41e646f83df8d15785ff1c90e90bde5
vpoletae <43249448+vpoletae@users.noreply.github.com>
2019-02-08 19:33:32 +0300

    Add files via upload

--- Post.py
+++ Post.py
@@ -1,17 +1,21 @@
 import email
 import smtplib
 import imaplib
-from email.MIMEText import MIMEText
-from email.MIMEMultipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 class Mailer:
-      def __init__(self, sender_mail, password):
-            self.GMAIL_SMTP = "smtp.gmail.com"
-            self.GMAIL_IMAP = "imap.gmail.com"
+      def __init__(self, sender_mail: str, password: str,
+                         smtp = GMAIL_SMTP, imap = GMAIL_IMAP,
+                         port_name = '587', inbox = 'inbox'):
+            self.smtp = smtp
+            self.imap = imap
+            self.port_name = port_name
             self.sender_mail = sender_mail
             self.password = password
+            self.inbox = inbox
 
-      def send_message(subject: str, recipients: list, body: str, header: str):
+      def send_message(self, subject: str, recipients: list, body: str):
             """send message"""
             message = MIMEMultipart()
             message['From'] = self.sender_mail
@@ -19,7 +23,7 @@ class Mailer:
             message['Subject'] = subject
             message.attach(MIMEText(body))
 
-            ms = smtplib.SMTP(self.GMAIL_SMTP, 587)
+            ms = smtplib.SMTP(self.smtp, self.port_name)
             # identify ourselves to smtp gmail client
             ms.ehlo()
             # secure our email with tls encryption
@@ -30,29 +34,29 @@ class Mailer:
             ms.sendmail(self.sender_mail, ms, message.as_string())
             ms.quit()
 
-      def recieve_message(header: str):
+      def recieve_message(self, header: str = ''):
             """recieve message"""
-            mail = imaplib.IMAP4_SSL(self.GMAIL_SMTP)
+            mail = imaplib.IMAP4_SSL(self.smtp)
             mail.login(self.sender_mail, self.password)
             mail.list()
-            mail.select("inbox")
+            mail.select(self.inbox)
 
-            criterion = ''
-            if header:
-                  criterion = f'HEADER Subject "{header}s"'
-            else:
-                  criterion = 'ALL'
+            criterion = f'(HEADER Subject "{header or "ALL"}")'
 
             result, data = mail.uid('search', None, criterion)
             assert data[0], 'There are no letters with current header'
             latest_email_uid = data[0].split()[-1]
             result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
             raw_email = data[0][1]
-            email_message = email.message_from_string(raw_email)
+            email_message = email.message_from_bytes(raw_email)
             mail.logout()
 
+            return email_message
+
 if __name__ == '__main__':
-      mailer = Mailer('login@gmail.com', 'qwerty')
-      mailer.send_message('Subject', ['vasya@email.com', 'petya@email.com'], 'Message', None)
-      mailer.recieve_message(None)
+      mailer = Mailer('login@gmail.com', 'qwerty',
+                            'smtp.gmail.com','imap.gmail.com')
+      mailer.send_message('Subject',
+                          ['vasya@email.com', 'petya@email.com'], 'Message', None)
+      mailer.recieve_message()
       
